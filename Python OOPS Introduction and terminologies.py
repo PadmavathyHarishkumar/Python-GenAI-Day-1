d434e59a5ea8baf2418f2c99cf001a7a2035af4c
PadmavathyHarishkumar <harishkumar836812@gmail.com>
2026-08-23 17:21:21 +0530

    Uploaded_Python_Terminologies

---
+++ Python OOPS Introduction and terminologies.py
@@ -0,0 +1,50 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#OOPS
+#Object oriented programming system
+
+#Goals/Benefits of OOPS:
+#1.With OOPS user can use an app for a longer period of time
+#2.It improves code optimization
+#3.It improves network security and information security
+#4.It improves MODULARITY(Seperating a bigger code into smaller code blocks)
+#5.It improves SCALABILITY(Handling number of users and thier loads)
+#6.It improves MAINTABILITY(App should work as expected/guranteed over years)
+
+#Problem before OOPS:
+#1.Code messy
+#2.Code complexity
+#3.Unable to find and handle errors
+#4.Time complexity
+
+#Properties/Pillars of OOPS:
+#1.Class
+#2.Objects
+#3.Data Abstaraction
+>>> #4.Data Encapsulation
+>>> #5.Data Inheritance
+>>> #6.Data Polymorphism
+>>> 
+>>> #Above all are common properties of OOPS
+>>> 
+>>> #Python defined properties of OOPS:
+>>> #1.Abstract class(ABC)
+>>> #2.__init__ initializer
+>>> #3.self special keyword
+>>> 
+>>> #What is a class:
+>>> #1.Class is a functional container/Methods container
+>>> #2.Class is a set of predefined rules and its setup
+>>> #3.Class is a blueprint of objects
+>>> #4.Class is a collection of code blocks
+>>> #5.Class is a collection of different states and its behaviour
+>>> #6.Class is a collection of properties and its attribute values
+>>> #7.Class is a collection of internal references of itself
+>>> 
+>>> #Rules of class:
+>>> #1.Class keyword must be used
+>>> #2.Class name should start with uppercase
+>>> #3.Self keyword is mandate
+>>> 
+>>> #Objects:
+>>> #Objects are instance of class
